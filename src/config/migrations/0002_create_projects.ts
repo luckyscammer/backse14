e47fcb1340262ed81c888b7b8e64f9bd6d0d9192
Pliffdax <stepanov-oleksandr@ukr.net>
2025-02-11 14:10:33 +0200

    Added description field

--- src/config/migrations/0002_create_projects.ts
+++ src/config/migrations/0002_create_projects.ts
@@ -5,6 +5,7 @@ export const createProjectsTable = async () => {
     CREATE TABLE IF NOT EXISTS projects (
       id SERIAL PRIMARY KEY,
       name TEXT NOT NULL,
+      description TEXT,
       owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
       created_at TIMESTAMP DEFAULT NOW()
     );
